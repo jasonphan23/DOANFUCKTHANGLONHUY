5a29b0bf1e364d4a86ab5b87c824e2c6ca242660
nhieunguyen1510 <vannhieunguyen1994@gmail.com>
2017-03-27 14:58:23 +0700

    Add Document directory and Move source code to other folder

--- SourceCode/View/danhsachphieunhaphangview.cs
+++ SourceCode/View/danhsachphieunhaphangview.cs
@@ -6,14 +6,17 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Model.ENTITY;
 
 namespace WindowsFormsApplication1.View
 {
     public partial class danhsachphieunhaphangview : UserControl
     {
+        Book book;
         public danhsachphieunhaphangview()
         {
             InitializeComponent();
+            this.book = new Book();
         }
     }
 }
